845361e2bec53f63046dbecb6e6f8e70432ede20
Drake B <drakeboard719@riversideunified.org>
2024-10-17 09:14:01 -0700

    Created player movement

---
+++ Steampunk Puzzle Platformer Game/Assets/Drake/DrakeScripts/DrakePlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrakePlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 15f;
+
+    private Rigidbody2D _rigidbody2D;
+    private CapsuleCollider2D _capsuleCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        _rigidbody2D.velocity = new Vector2(horizontalInput * moveSpeed, _rigidbody2D.velocity.y);
+    }
+}
