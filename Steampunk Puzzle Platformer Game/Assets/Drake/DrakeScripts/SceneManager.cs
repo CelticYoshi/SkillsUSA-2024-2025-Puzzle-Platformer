e249c7e0978504854df57718d61d98257f0d060a
Drake B <drakeboard719@riversideunified.org>
2024-11-01 09:24:41 -0700

    Updated scene management

---
+++ Steampunk Puzzle Platformer Game/Assets/Drake/DrakeScripts/SceneManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public int levelSelectScene;
+    public int controlsScene;
+    public int titleScene;
+    public int levelOneScene;
+    public int levelTwoScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnLevelSelectPressed()
+    {
+       SceneManager.LoadScene(levelSelectScene);
+    }
+
+    public void OnControlsPressed()
+    {
+       SceneManager.LoadScene(controlsScene);
+    }
+
+    public void OnBackPressed()
+    {
+       SceneManager.LoadScene(titleScene);
+    }
+
+    public void OnMainMenuPressed()
+    {
+       SceneManager.LoadScene(titleScene);
+    }
+
+    public void OnOnePressed()
+    {
+        SceneManager.LoadScene(levelOneScene);
+    }
+
+    public void OnTwoPressed()
+    {
+        SceneManager.LoadScene(levelTwoScene);
+    }
+}
+
