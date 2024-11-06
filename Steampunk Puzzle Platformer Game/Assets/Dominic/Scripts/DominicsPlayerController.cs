efd3fdeccf2e51ed3bd17a63f15de437ed0c329d
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2024-11-06 09:16:44 -0800

    Working on animations

--- Steampunk Puzzle Platformer Game/Assets/Dominic/Scripts/DominicsPlayerController.cs
+++ Steampunk Puzzle Platformer Game/Assets/Dominic/Scripts/DominicsPlayerController.cs
@@ -9,12 +9,14 @@ public class DominicsPlayerController : MonoBehaviour
 
     private Rigidbody2D _rigidbody2D;
     private CapsuleCollider2D _capsuleCollider2D;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,8 @@ public class DominicsPlayerController : MonoBehaviour
     private void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        _animator.SetFloat("movementValue", horizontalInput);
 
         _rigidbody2D.velocity = new Vector2(horizontalInput * movespeed, _rigidbody2D.velocity.y);
     }
